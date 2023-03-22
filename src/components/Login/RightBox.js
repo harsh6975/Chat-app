e08b952e482555d78893d917462f415a8ef4a414
Shyam Sundar Goyal <goyalshyamsundar2@gmail.com>
2023-03-22 11:30:40 +0530

    added color feedback on login-signup toogle

--- src/components/Login/RightBox.js
+++ src/components/Login/RightBox.js
@@ -8,18 +8,20 @@ import SignUp from './SignupBox';
 function RightBox() {
 
     const [toggleState, setToggleState] = useState(true);
+    const [isLoginActive, setIsLoginActive] = useState(true);
 
     function changeToggleState(toggle) {
         setToggleState(toggle);
+        setIsLoginActive(toggle);
     }
 
     return (
         <div className={style.right_box }>
             <div className={style.login_signup_toggle}>
-                <div className={style.login_toggle}>
+                <div className={`${style.login_toggle} ${isLoginActive ? style.bg_red : style.bg_grey}`}>
                     <p onClick={()=>changeToggleState(true)} className={style.login_toggle_btn}>Sign In</p>
                 </div>
-                <div className={style.signup_toggle}>
+                <div className={`${style.signup_toggle} ${!isLoginActive ? style.bg_red : style.bg_grey}`}>
                     <p onClick={()=>changeToggleState(false)} className={style.signup_toggle_btn}>Sign Up</p>
                 </div>
             </div>
